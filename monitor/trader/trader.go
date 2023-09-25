5ba559b5246fe137e4aa0ae66e1b5f2add00fc6d
qw3320400 <17098820947@163.com>
2023-09-25 17:30:31 +0800

    linea gas price

--- monitor/trader/trader.go
+++ monitor/trader/trader.go
@@ -236,10 +236,8 @@ func (t *Trader) finalCheck(gasUsed uint64, inputAmount float64, pairPath []*pro
 	}
 	if maxGasPrice < gasPrice {
 		gasPrice = minGasPrice
-	} else if fee > 0.001*math.Pow10(18) {
-		return 0, fmt.Errorf("final check danger amountIn %f amountOut %f gasUsed %d maxGasPrice %f gwei minGasPrice %f gwei eth gasPrice %f gwei", inputAmount/math.Pow10(18), amountOut/math.Pow10(18), gasUsed, maxGasPrice/math.Pow10(9), minGasPrice/math.Pow10(9), t.ETHGasPrice()/math.Pow10(9))
-	} else {
-		gasPrice = maxGasPrice
+		// } else if fee > 0.001*math.Pow10(18) {
+		// 	return 0, fmt.Errorf("final check danger amountIn %f amountOut %f gasUsed %d maxGasPrice %f gwei minGasPrice %f gwei eth gasPrice %f gwei", inputAmount/math.Pow10(18), amountOut/math.Pow10(18), gasUsed, maxGasPrice/math.Pow10(9), minGasPrice/math.Pow10(9), t.ETHGasPrice()/math.Pow10(9))
 	}
 	utils.Warnf("final check pass amountIn %f amountOut %f gasUsed %d passGasPrice %f gwei maxGasPrice %f gwei minGasPrice %f gwei eth gasPrice %f gwei", inputAmount/math.Pow10(18), amountOut/math.Pow10(18), gasUsed, gasPrice/math.Pow10(9), maxGasPrice/math.Pow10(9), minGasPrice/math.Pow10(9), t.ETHGasPrice()/math.Pow10(9))
 	return int64(gasPrice), nil
